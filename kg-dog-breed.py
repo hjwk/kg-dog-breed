187a9d8ddeae40f8f8e6d5a3496c300e1f3a4859
Hubert Woszczyk <woszczykh@gmail.com>
2017-12-22 16:10:44 +0100

    Cleaned the code a bit

--- kg-dog-breed.py
+++ kg-dog-breed.py
@@ -1,28 +1,24 @@
 from sklearn.datasets import load_files       
 
-from keras import applications  
+from keras import applications
 from keras.utils import np_utils
-from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img  
-from keras.layers import Dropout, Flatten, Dense, GlobalAveragePooling2D
+from keras.layers import Dropout, Flatten, Dense
 from keras.models import Sequential
 from keras.callbacks import ModelCheckpoint
-
 from keras.preprocessing import image                  
-from tqdm import tqdm
 
+from tqdm import tqdm
 from PIL import ImageFile                            
 ImageFile.LOAD_TRUNCATED_IMAGES = True  
 
 import pandas as pd
 import numpy as np
+import glob
+import os
 
 train_dir = 'data_gen/train'
 val_dir = 'data_gen/validation'
 test_dir = 'data/test'
-
-img_width = 224
-img_height = 224
-batch_size = 16
 
 def path_to_tensor(img_path):
     # loads RGB image as PIL.Image.Image type
@@ -36,23 +32,12 @@ def paths_to_tensor(img_paths):
     list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
     return np.vstack(list_of_tensors)
 
-## Bottleneck features
-def save_bottleneck_features():
-    #datagen = ImageDataGenerator(preprocessing_function=applications.vgg16.preprocess_input)
-
-    # build the network
-    model = applications.VGG16(include_top=False, weights='imagenet')
+def generate_bottleneck_features():
+    model = applications.ResNet50(include_top=False, weights='imagenet')
 
     train_files = load_files(train_dir)
-    train_tensors = paths_to_tensor(train_files['filenames'])#.astype('float32')/255
-    train_data = applications.vgg16.preprocess_input(train_tensors)
-
-    '''generator = datagen.flow_from_directory(
-        train_dir,
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        class_mode=None,
-        shuffle=False)'''
+    train_tensors = paths_to_tensor(train_files['filenames'])
+    train_data = applications.resnet50.preprocess_input(train_tensors)
         
     bottleneck_features_train = model.predict(
         train_data, batch_size=16)
@@ -60,20 +45,26 @@ def save_bottleneck_features():
     np.save('bottleneck_features/train.npy', bottleneck_features_train)
 
     val_files = load_files(val_dir)
-    val_tensors = paths_to_tensor(val_files['filenames'])#.astype('float32')/255
-    val_data = applications.vgg16.preprocess_input(val_tensors)
+    val_tensors = paths_to_tensor(val_files['filenames'])
+    val_data = applications.resnet50.preprocess_input(val_tensors)
     
-    #generator = datagen.flow_from_directory(
-    #    val_dir,
-    #    target_size=(img_width, img_height),
-    #    batch_size=batch_size,
-    #    class_mode=None,
-    #    shuffle=False)
-
     bottleneck_features_validation = model.predict(
         val_data, batch_size=16)
 
     np.save('bottleneck_features/validation.npy', bottleneck_features_validation)
+
+def generate_bottleneck_features_test():
+    # build the network
+    model = applications.ResNet50(include_top=False, weights='imagenet')
+
+    files = glob.glob('data/test/*.jpg')
+    tensors = paths_to_tensor(files)
+    data = applications.resnet50.preprocess_input(tensors)
+        
+    bottleneck_features = model.predict(
+        data, batch_size=16)
+
+    np.save('bottleneck_features/test.npy', bottleneck_features)
 
 def load_labels(path):
     data = load_files(path)
@@ -81,8 +72,11 @@ def load_labels(path):
 
     return labels
 
+def extract_Resnet50(tensor):
+	return ResNet50(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+
 ## Create bottleneck features
-#save_bottleneck_features()
+#generate_bottleneck_features()
 
 ## Load bottleneck features
 print('Loading training bottleneck features')
@@ -96,21 +90,41 @@ validation_labels = load_labels('data_gen/validation')
 ## Model definition
 print('Defining model')
 model = Sequential()
-model.add(GlobalAveragePooling2D(input_shape = train_data.shape[1:]))
-model.add(Dense(512, activation='relu'))
+model.add(Flatten(input_shape = train_data.shape[1:]))
 model.add(Dense(120, activation='softmax'))
 model.summary()
-
 model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
 
 ## Training
 checkpointer = ModelCheckpoint(filepath='saved_models/weights.best.hdf5', 
                                verbose=1, save_best_only=True)
-epochs = 15
+epochs = 45
 history = model.fit(train_data, train_labels,
                     validation_data=(validation_data, validation_labels),
                     epochs=epochs, batch_size=batch_size,
                     callbacks=[checkpointer], verbose=2)
 
 ## Testing
+'''
 model.load_weights('saved_models/weights.best.hdf5')
+
+train_labels = np.array(pd.read_csv('data/labels.csv'))
+classes, counts = np.unique(train_labels[:, 1], return_counts=True)
+
+f = open('results.csv', 'w')
+f.write('id')
+for c in classes:
+    f.write(',' + c)
+f.write('\n')
+
+#generate_bottleneck_features_test()
+test = np.load('bottleneck_features/test.npy')
+output = model.predict(test)
+filenames = os.listdir('data/test')
+for [o, name] in zip(output, filenames):
+    f.write(name[:-4] + ',')
+    o.tofile(f, sep=',', format='%.17f')
+    f.write('\n')
+
+f.close()
+'''
